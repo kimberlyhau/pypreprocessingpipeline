757b062988c55da7cb8b19dc8a8bd832ff091b77
Kim-Hau <61749817+Kim-Hau@users.noreply.github.com>
2020-07-19 17:22:40 -0400

    Create signal.py

---
+++ signal.py
@@ -0,0 +1,16 @@
+
+
+class Signal:
+    def __init__(self, ts, val):
+        self.ts = ts
+        self.val = val
+
+class CSVSignal:
+    def __init__(self, filename, batchsize):
+        self.batchsize = batchsize
+		
+
+    def next(self):
+	signal = Signal (,)
+	return signal
+	
